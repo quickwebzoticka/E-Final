fd560bf996e3e6502c98b6bf9296c6ebf3bc9516
JadenKoor <dazekamikadze19962002@gmail.com>
2019-09-28 13:50:10 +0300

    header auth

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-
-
+	if(document.title != 'Авторизация' && document.title != 'Карта'){
+		$(document).find('.footer').hide();
+		$(document).find('.header:not(.auth)').hide();
+	}
+	if(document.title == 'Авторизация' || document.title == 'Карта'){
+		$(document).find('.breadcrumbs').hide();
+		$(document).find('.header.auth').hide();
+	}
 });
 
