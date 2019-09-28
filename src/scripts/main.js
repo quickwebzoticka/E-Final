72313922c648d491eb959d837691bb5b5cafaeab
JadenKoor <dazekamikadze19962002@gmail.com>
2019-09-28 16:33:02 +0300

    pickup page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,17 @@ $(document).ready(function () {
 	}
 });
 
+$(document).on('click', '.sort', function () {
+	$(document).find('.sort-block').toggle();
+	$(this).find('span').toggleClass('active');
+});
+$(document).on('click', '.sort-block__item', function () {
+	$(document).find('.sort span span').text($(this).text());
+});
+$(document).on('click', function (e) { // событие клика по веб-документу
+	var div = $('.sort-block'); // тут указываем сласс элемента
+	if ( !div.is(e.target) && div.has(e.target).length === 0 && !$('.sort').is(e.target) && $('.sort').has(e.target).length === 0) { // если клик был не по нашему блоку и не по его дочерним элементам
+		$(document).find('.sort-block').hide();
+		$(this).find('span').removeClass('active');
+	}
+});
